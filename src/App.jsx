4c9c7eb703c4c2d1caac9ccff336197c4324b784
Grégoire Aubertin <greg_abrn@icloud.com>
2025-06-09 17:17:24 +0200

    react-counter-v2

--- src/App.jsx
+++ src/App.jsx
@@ -13,47 +13,51 @@ function App() {
         <h1>React Counter V2</h1>
       </header>
       <main>
-        <div className="div-add-counter">
-          {/* Dans le button ci dessous, lorsqu'on clique sur Add Counter, on va push un nouvel élément dans le tableau, 
+        <div>
+          <div className="add">
+            {/* Dans le button ci dessous, lorsqu'on clique sur Add Counter, on va push un nouvel élément dans le tableau, 
           qui lui même est dans un state. Ça veut dire qu'à chaque fois que l'on clique, on va créer un nouveau counter */}
-          <button
-            onClick={() => {
-              // 1 : Création de la copie du tableau [tab]
-              const newTab = [...tab];
-              // 2 : Modif de la copie
-              newTab.push(0);
-              // 3 : MaJ du state avec sa copie
-              setTab(newTab);
-            }}
-          >
-            Add Counter
-          </button>
+            <button
+              onClick={() => {
+                // 1 : Création de la copie du tableau [tab]
+                const newTab = [...tab];
+                // 2 : Modif de la copie
+                newTab.push(0);
+                // 3 : MaJ du state avec sa copie
+                setTab(newTab);
+              }}
+            >
+              Add Counter
+            </button>
+          </div>
           <section>
-            {/* .map ici */}
             {tab.map((elem, index) => {
               return (
-                <div className="div-counter">
-                  <FaMinus
-                    onClick={() => {
-                      setTab(tab - 1);
-                    }}
-                  />
+                <div>
+                  <div className="div-counter">
+                    <FaMinus
+                      onClick={() => {
+                        setTab(tab - 1);
+                      }}
+                    />
 
-                  <p>{tab}</p>
+                    <p>{tab[0]}</p>
 
-                  <FaPlus
-                    onClick={() => {
-                      setTab(tab + 1);
-                    }}
-                  />
-
-                  <button
-                    onClick={() => {
-                      setTab(0);
-                    }}
-                  >
-                    Reset
-                  </button>
+                    <FaPlus
+                      onClick={() => {
+                        setTab(tab + 1);
+                      }}
+                    />
+                  </div>
+                  <div className="reset">
+                    <button
+                      onClick={() => {
+                        setTab(0);
+                      }}
+                    >
+                      Reset
+                    </button>
+                  </div>
                 </div>
               );
             })}
